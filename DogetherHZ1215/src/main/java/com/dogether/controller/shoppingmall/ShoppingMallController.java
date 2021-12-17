05f5c4ba2c2351f30e62ee9999928f2add91da7f
heejin <hizzzzzzzin29@gmail.com>
2021-12-17 17:52:06 +0900

    ㅎㅎ

---
+++ DogetherHZ1215/src/main/java/com/dogether/controller/shoppingmall/ShoppingMallController.java
@@ -0,0 +1,8 @@
+package com.dogether.controller.shoppingmall;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+public class ShoppingMallController {
+
+}
